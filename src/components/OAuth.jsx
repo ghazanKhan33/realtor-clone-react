0219f97aac31b845ff2aa54268220f60e21e4090
Ghazan Khan <ghazan.elitx@gmail.com>
2023-04-13 15:35:13 +0500

    google oAuth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import {FcGoogle} from 'react-icons/fc'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const OAuth = () => {
-  return (
-    <button className='w-full flex gap-2 justify-center items-center bg-red-600 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-700 active:bg-red-800 hover:shadow-lg transitio duration-300 ease-in-out rounded'><FcGoogle className='text-2xl bg-white rounded-full' /> Continue with Google</button>
-  )
-}
+  const navigate = useNavigate();
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
 
-export default OAuth
+      // check whethte the user already exists
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if(!docSnap.exists()){
+        await setDoc(docRef,{
+          name: user.displayName,
+          email: user.email,
+          timeStamp: serverTimestamp()
+        })
+      }
+      navigate('/');
+    } catch (error) {
+      toast.error("Could not authorize with Google")
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="w-full flex gap-2 justify-center items-center bg-red-600 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-700 active:bg-red-800 hover:shadow-lg transitio duration-300 ease-in-out rounded"
+    >
+      <FcGoogle className="text-2xl bg-white rounded-full" /> Continue with
+      Google
+    </button>
+  );
+};
+
+export default OAuth;
